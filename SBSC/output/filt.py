bf00693d419363ecc74f31b0ac1f4b90917e4927
delocalizer <conrad.leonard@hotmail.com>
2021-09-15 12:27:20 +1000

    linting, bump pre-release version to 0.0.13

--- SBSC/output/filt.py
+++ SBSC/output/filt.py
@@ -1,25 +1,10 @@
-#!/usr/bin/env python3
-import json
-import os
-#from pstats import SortKey
-from glob import glob
-import gzip
-import pysam
-from collections import Counter, defaultdict
+from collections import defaultdict
 import pandas as pd
-import argparse
-#import cProfile
-import sys
-import time
-from subprocess import run
 
-def main (args):
-
-   pass
 
 def filt(d, args, chroms):
 
-    d2=defaultdict(lambda: defaultdict(list))
+    d2 = defaultdict(lambda: defaultdict(list))
     for pos, calls in d.items():
         for k, v in calls.items():
             if type(v) != list:
@@ -28,52 +13,48 @@ def filt(d, args, chroms):
                 for i, option in enumerate(v):
                     p = args.P_value
                     alt = calls.get('tumour_alts')[i]
-                    percent_norm = calls.get('read_count_normal')[i][0]/calls.get('read_count_normal')[i][1]
-                    percent_tumour = calls.get('read_count_tumour')[i][0]/calls.get('read_count_tumour')[i][1]
+                    percent_norm = calls.get('read_count_normal')[i][0] /\
+                        calls.get('read_count_normal')[i][1]
                     if percent_norm > 0.07:
                         continue
                     if alt[0] in ['-', '+']:
                         if percent_norm > 0.00:
                             continue
-                    if alt == calls.get('ref'):# any LOH that slip through that arent interesting
-                        continue #
+                    if alt == calls.get('ref'):  # any LOH that slip through that arent interesting
+                        continue
                     if calls.get('read_count_tumour')[i][1] > 120 and \
                             calls.get('read_count_normal')[i][1] > 60:
-                                p*=0.001 #if in CNV requires lower P
+                        p *= 0.001  # if in CNV requires lower P
                     if calls.get('read_count_tumour')[i][1] > 480 and \
                             calls.get('read_count_normal')[i][1] > 240:
-                                p*=0.00001 #if in CNV requires lower P
-                    if 0.07 > calls.get('strand')[i] or calls.get('strand')[i] > 0.93:
+                        p *= 0.00001  # if in CNV requires lower P
+                    if 0.07 > calls.get('strand')[i] or \
+                            calls.get('strand')[i] > 0.93:
                         continue
                     if float(calls.get('tumour_P')[i]) > p:
                         continue
                     if calls.get('read_count_tumour')[i][1] < args.min_depth_tumour or \
                         calls.get('read_count_normal')[i][1] < args.min_depth_normal:
-                        break#HOM dels that actually line up properly should have read depth zero
+                        break  # HOM dels that actually line up properly should have read depth zero
                     if all(map(lambda x: x > args.min_mean_base_quality, [
                             float(calls.get('tumour_qual')[i]),
                             calls.get('tumour_qual_all'),
                             calls.get('normal_qual_all')])):
-                            d2[pos][k].append(option)
+                        d2[pos][k].append(option)
         if not d2[pos].get('tumour_alts'):
             del d2[pos]
     df = pd.DataFrame.from_dict(d2, orient='index')
 
-
-    #start = time.time()
     if len(df):
-        # del df['index_col']
-        # del df['chrom']
-        # del df['pos']
         if args.MNVs:
             df = MNVs(df)
-        #end = time.time()
-        fout_name = '_'.join([args.output,
+        fout_name = '_'.join([
+            args.output,
             str(args.P_value).split('.')[1],
             str(args.min_mean_base_quality),
             str(args.min_depth_tumour),
             str(args.min_depth_normal)])
-        #get rid of lsits
+        # get rid of lsits
         df = df.explode('tumour_alts')
         df = df.explode('tumour_P')
         df = df.explode('tumour_qual')
@@ -84,42 +65,38 @@ def filt(d, args, chroms):
 
         df.to_csv(fout_name + '.tsv', sep='\t')
 
+
 def MNVs(df):
 
     df['index_col'] = df.index
-    df[['chrom','pos']] = df.index_col.str.split(':', expand=True)
+    df[['chrom', 'pos']] = df.index_col.str.split(':', expand=True)
     df.pos = df.pos.astype(int)
     already_updated = set([])
     for chrom, df_tmp in df.groupby('chrom'):
         df_tmp = df_tmp.sort_values(by=['pos'])
         for var_len in [5, 4, 3, 2, 1]:
             df_tmp['dif'] = df_tmp.pos.diff(var_len)
-            hits=df_tmp[df_tmp['dif'] == var_len]
-            if len(hits):#not empty
+            hits = df_tmp[df_tmp['dif'] == var_len]
+            if len(hits):  # not empty
                 for hit in hits.pos:
                     rows = df_tmp[(df_tmp.pos <= hit) & (df_tmp.pos >= (hit - var_len))]
                     update = dict(rows.iloc[0])
                     if len(update.get('tumour_alts')) == 1 and len(update.get('tumour_alts')[0]) == 1:
                         for i in range(var_len):
-                            i+=1
+                            i += 1
                             update_tmp = dict(rows.iloc[i])
                             if len(update.get('tumour_alts')) == 1 and len(update.get('tumour_alts')[0]) == 1:
                                 key = update_tmp.get('chrom') + ':'+str(update_tmp.get('pos'))
                                 if key not in already_updated:
-                                    update['tumour_alts'][0]+=update_tmp.get('tumour_alts')[0]
-                                    update['tumour_P']+=update_tmp.get('tumour_P')
-                                    update['tumour_qual']+=update_tmp.get('tumour_qual')
-                                    update['normal_qual']+=update_tmp.get('normal_qual')
-                                    update['strand']+=update_tmp.get('strand')
-                                    update['read_count_tumour']+=update_tmp.get('read_count_tumour')
-                                    update['read_count_normal']+=update_tmp.get('read_count_normal')
+                                    update['tumour_alts'][0] += update_tmp.get('tumour_alts')[0]
+                                    update['tumour_P'] += update_tmp.get('tumour_P')
+                                    update['tumour_qual'] += update_tmp.get('tumour_qual')
+                                    update['normal_qual'] += update_tmp.get('normal_qual')
+                                    update['strand'] += update_tmp.get('strand')
+                                    update['read_count_tumour'] += update_tmp.get('read_count_tumour')
+                                    update['read_count_normal'] += update_tmp.get('read_count_normal')
                                     df = df.drop(index=(key))
                                     already_updated.add(key)
                             else:
                                 break
-
     return df
-
-
-if __name__ == '__main__':
-    main()
--- bin/SBSCall.py
+++ bin/SBSCall.py
@@ -1,22 +1,17 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-from multiprocessing import Pool, TimeoutError
+from multiprocessing import Pool
 import argparse
 import sys
 import json
 import pandas as pd
-import os
-from glob import glob
 
-#import time
-#from subprocess import run#call
 import SBSC.parsers.parse_pile as pp
 import SBSC.output.filt as ff
-#import liams_simple_scaffold_pkg.prefil.prefil_files as fil
+
 
 def parse_args(args):
-
 
     parser = argparse.ArgumentParser(description='somatic var caller')
 
@@ -35,7 +30,7 @@ def parse_args(args):
         '--output',
         type=str,
         help='output name [output]',
-        default = 'output')
+        default='output')
 
     required.add_argument(
         '-c',
@@ -72,7 +67,7 @@ def parse_args(args):
         '--base_qual',
         type=int,
         help='Minimum individual base qual to use for var calling. [7]',
-        default=7)#10=10% error
+        default=7)  # 10=10% error
 
     optional.add_argument(
         '-w',
@@ -90,49 +85,49 @@ def parse_args(args):
         '--min_mean_base_quality',
         type=int,
         help='Minimum mean base quality per position [9]',
-        default = 9)
+        default=9)
 
     filters.add_argument(
         '-p',
         '--P_value',
         type=float,
         help='Exclude P values (from Fishers Exact test) larger than [0.001]',
-        default = 0.001)
+        default=0.001)
 
     filters.add_argument(
         '-m',
         '--min_depth_tumour',
         type=int,
         help='Minimun read depth for tumour [12]',
-        default = 12)
+        default=12)
 
     filters.add_argument(
         '-y',
         '--min_depth_normal',
         type=int,
         help='Minimun read depth for normal [10]',
-        default = 10)
+        default=10)
 
     filters.add_argument(
         '-j',
         '--json',
         action='store_false',
         help='Make a json of all raw results to allow re-run of filtering [True].',
-        default = True)
+        default=True)
 
     filters.add_argument(
         '-u',
         '--use_json',
         action='store_true',
         help='Use a json of all raw results to re-run filtering [False].',
-        default = False)
+        default=False)
 
     filters.add_argument(
         '-z',
         '--MNVs',
         action='store_false',
         help='Call MNVs (join contigous SNVs in output) [True].',
-        default = True)
+        default=True)
 
     # annotate = parser.add_argument_group(
     #     'Annotation',
@@ -171,57 +166,43 @@ def parse_args(args):
     #     type=str,
     #     help='telomeres_and_centromeres_38',
     #     default = '/working/lab_nicw/liamM/nano/COLO829/pile/telomeres_and_centromeres_38.txt')
-
     return parser.parse_args(args)
 
-def main():
 
+def main():
     '''
     Make a simple python package scaffold
     '''
 
     args = parse_args(sys.argv[1:])
-    #pp.main(args)
 
-    ####Several columns contain numeric quality values encoded as individual ASCII characters. Each character can range from “!” to “~” and is decoded by taking its ASCII value and subtracting 33; e.g., “A” encodes the numeric value 32.
-    #start_total = time.time()
+    # Several columns contain numeric quality values encoded as individual ASCII characters. Each character can range from “!” to “~” and is decoded by taking its ASCII value and subtracting 33; e.g., “A” encodes the numeric value 32.
     if args.chrom == 'all':
-            chroms = ['chr' + str(i+1) for i in range(22)] + ['chrX', 'chrY']
+        chroms = ['chr' + str(i+1) for i in range(22)] + ['chrX', 'chrY']
     else:
         chroms = args.chrom.split(',')
-            
+
     if args.use_json:
         with open('result.json') as f:
             d = json.load(f)
     else:
-        chunks = pp.chunk_ref(args, chroms)#[211:216] #testing
-        print (f'len chunks {len(chunks)}')
-        d={}
+        chunks = pp.chunk_ref(args, chroms)
+        print(f'len chunks {len(chunks)}')
+        d = {}
         with Pool(processes=args.threads) as pool:
             tmp = [(args, chunk) for chunk in chunks]
             res = pool.map(pp.doit, tmp)
             for variants in res:
                 d = {**d, **variants}
-        print ('d',len(d))
+        print('d', len(d))
         if args.json:
             with open('result.json', 'w') as fout:
                 json.dump(d, fout)
-        
-    #end = time.time()
-    #p#rint('run time', end - start_total)
-    #start_total = time.time()
-    #ff.filt(d, 0.01, 9, 15, args, chroms, 12)
-    ff.filt(d, args, chroms)
-    #ff.filt(d, 0.0001, 9, 15, args, chroms, 12)
 
+    ff.filt(d, args, chroms)
     df = pd.DataFrame.from_dict(d, orient='index')
     df.to_csv(args.output + '.tsv', sep='\t')
-    #end = time.time()
-    #print('filt time', end - start_total)
-
 
 
 if __name__ == "__main__":
     main()
-
-
